db2c9af3811d72df68115b5212c9a3edd5f60fef
AlexanderB98 <alexanderburrosquieta46@gmail.com>
2022-06-12 19:58:16 -0500

    worked on to do

--- todo.js
+++ todo.js
@@ -1,30 +1,39 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+},
+{
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+}]
 
-    let arrayOfTodos = [
-      {
-      "userId": 14,
-      "id": 1,
-      "title": "delectus aut autem",
-      "completed": false
-  },
-  {
-      "userId": 20,
-      "id": 2,
-      "title": "delectus aut autem",
-      "completed": false
-  }]
-  
-  const fetchTodos = () => {
-      fetch('https://jsonplaceholder.typicode.com/todos')
-      .then( (response) => response.json())
-      .then( (json) => arrayOfTodos = json)
-  }
-  
-  const logTodos = () => {
-      console.log(arrayOfTodos)
-  }
-  
-  const populateTodos = () => {
-    let onlyIds = arrayOfTodos.ma
-  
-  }
+// let 
+
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => {
+        console.log('response', response)
+        return response.json()  
+    })
+    .then((data) => {
+        console.log('data', data)
+        arrayOfTodos = data
+    })
+}
+const logTodos = () => {
+    console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+    console.log('arrayOfTodos:', arrayOfTodos)
+    let toDoList = document.getElementById("todo-list")
+    let text = document.createElement("li")
+    text.appendChild(document.createTextNode(arrayOfTodos[0].userId))
+    toDoList.appendChild(text)
+    
+}
